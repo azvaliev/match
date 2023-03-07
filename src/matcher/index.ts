6bb7a5def5c55c59471a2f4247f3f77f092da29a
azat valiev <azvaliev@gmail.com>
2023-03-06 20:34:43 -0600

    clarifying comments

--- src/matcher/index.ts
+++ src/matcher/index.ts
@@ -10,8 +10,10 @@ function match(val: number, matcherOptions: Matcher<number>): void;
   * Match a string using RegExp pattern matching or literals
 * */
 function match(val: string, matcherOptions: Matcher<string>): void;
-function match<T>(val: T, matcherOptions: Matcher<T>): void {
+function match<T extends string | number>(val: T, matcherOptions: Matcher<T>): void {
   // Check val type to determine matcher type to use
+  // We can asset based on val type, that corresponding matcher options are valid
+  // as they are based on the same generic
 
   if (typeof val === 'string') {
     return stringMatcher(val, matcherOptions as Matcher<string>);
